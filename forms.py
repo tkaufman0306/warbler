45bf2b725c03d7f5d6422b239814c80f53053f07
Taylor <taylorkkaufman@gmail.com>
2024-06-13 17:12:08 -0600

    first commit

---
+++ forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+class EditProfileForm(FlaskForm):
+    """edit user profile """
+
+    username = StringField('Username', validators=[DataRequired(), Length(min=1, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    image_url = StringField('Image URL', validators=[DataRequired()])
+    header_image_url = StringField('Header Image URL', validators=[DataRequired()])
+    bio = StringField('Bio', validators=[Length(max=150)])
+    Password = PasswordField('Password', validators=[DataRequired()])
